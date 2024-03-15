61743e84998af301d646d92ec11c18315661c301
Kay Roepke <kroepke@googlemail.com>
2024-03-15 17:54:01 +0100

    add 2.11.1 on jdk17 and 21

--- send_data.sh
+++ send_data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-JDK21_HOST="https://admin:La6ohgaighei@127.0.0.1:9201"
 JDK17_HOST="https://admin:La6ohgaighei@127.0.0.1:9202"
-MESSAGES=25
+JDK21_HOST="https://admin:La6ohgaighei@127.0.0.1:9201"
+OS211_JDK17_HOST="https://admin:La6ohgaighei@127.0.0.1:9204"
+OS211_JDK21_HOST="https://admin:La6ohgaighei@127.0.0.1:9203"
+MESSAGES=33
 RUNS=50
 
 long_string=$(head -c 1000000 /dev/zero | tr '\0' '\141')
@@ -37,6 +39,35 @@ echo -n "Creating test index (0 replicas, 1 shard): "; curl -k -XPUT $JDK21_HOST
 for run in $(seq 1 $RUNS); do
     echo "Request $run"
     reply=$(curl -s $JDK21_HOST/_bulk -k --data-binary @$tmp_file --header "Content-Type: application/json")
+    case "$reply" in 
+        *"circuit"* ) echo "$reply";;
+        * ) echo "Ok";;
+    esac
+done
+
+echo 
+echo "Running $RUNS bulk request of $MESSAGES large 1MB messages against 2.11.1 on JDK17:"
+echo 
+
+echo -n "Deleting test index: "; curl -k -XDELETE $OS211_JDK17_HOST/test; echo
+echo -n "Creating test index (0 replicas, 1 shard): "; curl -k -XPUT $OS211_JDK17_HOST/test -d '{"settings": {"index": {"number_of_shards": 1,"number_of_replicas": 0}}}' --header "Content-Type: application/json" ; echo
+for run in $(seq 1 $RUNS); do
+    echo "Request $run"
+    reply=$(curl -s $OS211_JDK17_HOST/_bulk -k --data-binary @$tmp_file --header "Content-Type: application/json")
+    case "$reply" in 
+        *"circuit:"* ) echo "$reply";;
+        * ) echo "Ok";;
+    esac
+done
+
+echo 
+echo "Running $RUNS bulk request of $MESSAGES large 1MB messages against 2.11.1 on JDK21 (default container):"
+echo
+echo -n "Deleting test index: "; curl -k -XDELETE $OS211_JDK21_HOST/test ; echo
+echo -n "Creating test index (0 replicas, 1 shard): "; curl -k -XPUT $OS211_JDK21_HOST/test -d '{"settings": {"index": {"number_of_shards": 1,"number_of_replicas": 0}}}' --header "Content-Type: application/json" ; echo
+for run in $(seq 1 $RUNS); do
+    echo "Request $run"
+    reply=$(curl -s $OS211_JDK21_HOST/_bulk -k --data-binary @$tmp_file --header "Content-Type: application/json")
     case "$reply" in 
         *"circuit"* ) echo "$reply";;
         * ) echo "Ok";;
